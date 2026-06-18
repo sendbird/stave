b43e6761e1e11784f69daf91a1b484d33f69d2bc
Jacob Kim <jacob.kim@sendbird.com>
2026-06-18 14:17:54 +0900

    fix(layout): align information panel item padding

--- src/components/layout/WorkspaceInformationPanel.tsx
+++ src/components/layout/WorkspaceInformationPanel.tsx
@@ -440,7 +440,7 @@ function SectionHeader(props: {
           </div>
         ) : null}
       </div>
-      <AccordionContent className="-mx-1 pb-3 pt-0">
+      <AccordionContent className="pb-3 pl-6 pt-0">
         {props.children}
       </AccordionContent>
     </AccordionItem>
